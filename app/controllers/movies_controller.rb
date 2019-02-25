924c56a15ad94a18363eacc5ad1252c35ca17109
EC2 Default User <ec2-user@ip-172-31-22-129.us-east-2.compute.internal>
2019-02-25 10:57:55 +0000

    Part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,10 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.ratings
     @sort = params[:sort]
-    @movies = Movie.all.order(@sort)
+    params[:ratings].nil ? @t_param = @all_ratings : @t_param = params[:ratings].keys
+    @movies = Movie.where(rating: @t_param).order(@sort)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+    
+    def self.ratings
+        Movie.select(:rating).distinct.inject([]) {|a, m| a.push m.rating}
+    end
 end
